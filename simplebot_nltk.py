740df62f314018f68680b8e2af724d4749329575
Jeff Bacolado <jeffconradbacolado@gmail.com>
2023-11-03 05:30:01 +0000

    Finished The Simple Bots

--- simplebot_nltk.py
+++ simplebot_nltk.py
@@ -5,8 +5,24 @@ from tkinter import *
 
 
 # Generating response
+from nltk.chat.util import Chat
+
+
+# Generating response
 def get_bot_response(user_input):
-   
+  pairs = [
+    ('my name is (.*)', ['Hello ! % 1']),
+    ('(hi|hello|hey|holla|hola)', ['Hey there !', 'Hi there !', 'Hey !']),
+    ('(.*) your name ?', ['My name is Charlie', 'I\'m Charlie the Chatbot']),
+    ('(.*) do you do ?', ['I specialize in regex pattern matching! How about you?']),
+    ('(.*) created you ?', ['You did! Using python, NLTK and tkinter! ']),
+    ('(.*) your day', ['It was good, thanks for asking!', 'Ah, I had a long day at the office... how about you?'])
+  ]
+
+  chat = Chat(pairs)  
+  while user_input[-1] in "!.":
+    user_input = user_input[:-1]
+  bot_response = chat.respond(user_input)   
   return bot_response
 
 
